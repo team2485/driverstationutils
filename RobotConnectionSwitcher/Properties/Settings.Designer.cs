917b08885e82c658f9f622251b8ec6cdc9d8a435
Bryce M <binarycaveman@gmail.com>
2013-06-25 12:47:32 -0700

    Updating default robot image to Sun Tzu.

--- RobotConnectionSwitcher/Properties/Settings.Designer.cs
+++ RobotConnectionSwitcher/Properties/Settings.Designer.cs
@@ -37,30 +37,38 @@ namespace RobotConnectionSwitcher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("iVBORw0KGgoAAAANSUhEUgAAAD0AAABACAYAAACp3n/2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8Y" +
-            "QUAAAUmSURBVGhD5dtXiCRVFIDhNWAOq5hFUVkTgogoBsSEYsIIRkwYHtwHQURREEVBDJgwgBEFFcTIK" +
-            "mJY1AdzBlFRVNQ155zD+P89Xcud2jPTO9N1OowHPmb6VPV03emqWzfVjJGRkV46Gl/hEszGAVgF0b5pw" +
-            "mSiy2FciMNGfx25E9G+acJkovtQxU/tn08h2jdNmEx0B/7BK/gFxttYEtH+KcJkogfwcvv3lfEYLPSi7" +
-            "VxPhMlEFtiCV68vxeeYtt+0BfsMVxW5U/EnVi9y6cJkktVgAU8vcofC2BjlvqnCZJJNYRyMKrejCWInl" +
-            "PumCpNJdoWxA6qc37DhN17umypMJjkKxixUuVXxB7y2y31Thckk5+BXrFTklsIXuKzIpQuTSW7EPCxe5" +
-            "PQa7q7lUoXJJI/juVpOc/FMLZcqTCZ5HdE3ehPeR89aZWEywbL4BrbA6tvOhZ2PFYtcqjCZYE38jVOKX" +
-            "OV4/It1ilyqMJlgCxgHor5tTxhbor4tRZhMUBVsW9S3bQXDferbUoTJBCfCWA/1bZ7Wxgmob0sRJhOcj" +
-            "58xs8hVlscPOK/IpQqTCW7DB4huS4vgPXjrqm9LESYTPImJxsLcZiMl2ta4MNmw6pu8q8jV3Q4bL9G2x" +
-            "oXJBtmLuhnGeIVeGy/izSKXKkw2ZD98iCrugYOB62MtWIHtC88C4yVEf6dxYbJL1tDXoR4OFf0Ih36/g" +
-            "11Kc1UMbaF3xzuYSgxdoe1QOGXjQP5UY6gKvR2sebuNoSj0ErBbWF6X3cTAF9pe0/NoMpzf8p4efV6jw" +
-            "mQHdgGrGccm41N4S4s+s1FhsoMLkBHfwsGG6DMbFSY7eBZZ4XCSdUX0uY0JkxOw7/s9MsM57NRrO0xOY" +
-            "H/0Ik5D9PmNCJPj8L9vn7cX4ZTuGoiOo2thsrAu7Cg8jY/giGav4mREx9S1MFk4A/2KRxEdU9fCZNvSe" +
-            "AP9io+RMgEQJtt6VWmNF3ZB7XtHx9aVMNk2B/2OXRAdW1fCJDaHk+X9jnJRTmPCJC7GIIRrzJZBdIxTF" +
-            "iVdoGolMiixB6LjlONsR2CzItdRlHTJ0yDFrfC4HHs7CCfBnp7zYtvgIdjNXQz1soSqX1zl4+ofRyczO" +
-            "xRTCQcRn4CtNOM3OGS8Gzx2vyRnT/xpGY6DzdiLYJ1wC17FkZhfaAvazdhWv8Lh5U/wO6qWosNWV+BMH" +
-            "INr4a3PeTTz8wv9MKZLeDZ4VtwAJwQdmHAt6llwZdNeaBW66WGfQYzqTLDgsy30la2X/5+YZ6HtSXmxW" +
-            "xm49mO6hRWflfOX8BmSWa0Lu81GgLcBF8MMWg0+2XAlkzW4Y/KbYAN4fW+I8D6tah3IsEZ1b6+4StHpp" +
-            "kN8XW4obQRvBcMc16Askx0onxIat9AuSLeqd5ZxEMPO0F+jv44b1k8P4nqcDWdQWo9SRAUuOU5l088RS" +
-            "qdW+x0uqHU01jUsCzMq69nqetRH4KIAr/GOhS6tgJ3hP8CasNuYzJ3C6/FwuGzaTsY+WJh4F44AjSnLm" +
-            "BeT4PTL3rgab2GytzqXWmyPe1uvRr/B+2Fb2VtMPeorDavHIDqFqxwW6JqOeTFFzkhsDZt6rhKKDroK5" +
-            "8A8ECcAfa89J3tMy7VfO8zsLbM8kzw1y8+T/3RHZztF9N5GCl3nPdHVfz5i+DVs+jlebq/HRxm8TKL3l" +
-            "axIfQjVJrJnRLSPj0B0ilZbu26BRMM8eBsEGdM0/s1q5VIUPj4RvS+90NlcgXgsXoBP8nmHsbZuNUJiI" +
-            "zP+A+ej2S6RkTbqAAAAAElFTkSuQmCC")]
+        [global::System.Configuration.DefaultSettingValueAttribute("iVBORw0KGgoAAAANSUhEUgAAADEAAABACAYAAACz4p94AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8Y" +
+            "QUAAAcdSURBVGhD1Zp1jGxVEofRsGiwhbAEd3aR4O7yBwTbEGSDBgIESHALDgFCAsHdZdGFEFzDLu4Q+" +
+            "APfhcey+9DgzvB9TdfLuXeqe2a675v35pd8kz7Vc6X6nnOqTp07xcDAwOTEZrBVzTYkqXGUmBZ2gFXbb" +
+            "AynwN9hKlgatoTs2AqpcSIyHcwJ68JZkOk72Bp8IhdAdp4KqXEisCJcBm/CJ/ANdNIv8O3vHwd+hlthG" +
+            "sjO2yI1Noxd49/Qq3RodsjO3SI1NsQMsCHcCL9CL/oAHOyOn+waLVJjjywGR8Kd8E94F3rVD/AG3A/Zt" +
+            "SqkxhHyB9gcPoMm9BpsCifAKpBds0Jq7IKPdYX257nhEHgVmtJNYPc7Hf4M9eunpMYuOEgPhDPhf9C0/" +
+            "EHOhoUgu35KahwCB1o/8kYfg/FtSr0Fy0B23Y6kxiHYB3qRQex92BemhoXhGPgrvAQGNsdXds2upMYuz" +
+            "AgGrJHoXvg/2P2OBVMLz+XNrwYbwX5tW0+kxi5sD0PpJwhHjc5ngP3cm70DjNwO2oNgTwgHdoVh5Up1U" +
+            "mMHpoSd4GGwWyjn869+/9jSl/AyHAr2fVMIdTCsDrvDx/AI3AJ/gzj/o2B0vhCMN2GfBex6HbtaahwGs" +
+            "4Jz+V9gPjAy/wdOAh2wy1wK34MOvw7jwCfk2NDZbSDOZ250D4TuhvjuRNA5k8awVUiNPWJ03bFm+xR0y" +
+            "qnZ8TQ/fA57Q/yPT/h20LnQPyC+v0QDWg/CViE19sjhcGXR9qaN4joRNlNxx0nZjWaDelb7EMT3F2lAo" +
+            "/Ik1oRXIJK1cOKwdjtsOrFz0XaarcvIHceEE+tA2Cqkxh7xhhzMS7bbZrE64RMq/6d0Yl54HpwgSl0Lc" +
+            "cx5GtBaELYKqbEPHBfOYH7WCcdE3Qlnp13AGcilqIHuRyjl+uO6NkZx5ZOO81RIjX1wHDgQ/Tw9+KuXY" +
+            "yKehPHGRNJZKpPTrbOfPKABrQFxngqpsQ+MwN6An/8E/sLnt9uyANh1DHS234ZMZXdyqlbGmbBVSI19Y" +
+            "Bd6EeYBb1hdD/H9UhrQyWD7nVZrsMpjnPGUP1DYKqTGPjFQGcjmAlX+qktoQAYw2/XVn4HRpWzmhGWds" +
+            "FVIjX1yFJgvubhXw3XCm3f8+CRNSeKYcKLjKi819sn6YH5lOqKcYeK7cMKlp+16FcRk8F9gmSaOCScs+" +
+            "4StQmrsk5nAwe28Xu8adSfMt0rtBk/CzRDH3AZqVJ2QG8D1twngSJwwNX8KyojtGFOxth9EamwA+/ZdY" +
+            "E5kQAu70Vx1csIU5BnwR4hjLAGp5SFsFVJjAzinm3pbhiyfROmE2WvdCVN40xCzWNP9P4LJoFoOymtMI" +
+            "DU2gJH5v6CugbBHnDgVrJgYDGPhpIzg2jzWbiQfgWNrWSivMYHU2BCxyCmn2HDiCDAJvA8e19CWs5IB0" +
+            "CnWFZ2pS5ynYxUkNTbEaaBKJ9xzUHYn19tOx5EbKXOvpyEbE64iw1YhNTaEN6jKMWGBwO5jfIglqFNqy" +
+            "HTkOSidcIJQHSuCqbEhvKgqZ6dwQm0A2pyNQkZynSgDZDhhVwxbhdTYEFYolLXVsDk4lePA2Ulb6YTdz" +
+            "O5UdsFwwhgTtgqpsSEWBZU5EVmslE4cDS/AVRDfx5iYbJzYRAOytBO20gnXGa70Lof4fpLFCVkEVDkmz" +
+            "ImU5cyw2X1CVtwtvlkljO9jCnZmC1uF1NgQC4LyJm2vBFEQcBfI6dQ6lN0n5JOwYlKuBs2llE82bBVSY" +
+            "wNYLPOiJoCW7w1e2U7S12DtNvQemG85BVvqdKay+mf64j53dq3GnbAcuRe4sHHbqrzBfqWDpiqDrjvI0" +
+            "AcWt8rANTHkUxq0HVxp9IillAeh123ekWpQObPSGCEmZFdAk11mOHINX7mXSmOYGIndQ4hXF0ZbZV7Vo" +
+            "tIYgpnBedzS5KSUaw2nZ+/FkumC2c2a0ywO5vvm9+4duCnYqVo3qfVS3Liv72wB54DBx/l9rGi8Dvhek" +
+            "bubY1XjdMJF+VjWRzphvx+r+hD2HqtO2P3dRrNo3ZpiXQOPJTlLum3gvbfwz8pwPLiuNffxEbnBbuY4O" +
+            "apc9U1woo6VN5O5L2ByVLkH2KLSKHCTpK5n4VxwfWBBy0pdVOdGQ25AujKcAyr3W2kUrA2l3O1/ApwE7" +
+            "JMuL13k+K6Gbwj4cokvHTYtz+kqbw9wCy27145O+D7S1eC4MFeydro/+CQuBpMwHfI1IN/b8HPUXutyf" +
+            "Lmg8VzOKmXttZs8ru93AM2hTLfdMLetYy5IfLXNIpibKNuCewqmxzrqHnUp0xcnCpebpuyuoev/U5fd1" +
+            "E398tWJrqTGHvCdDcvw7vSELAr4qp1OW+3bDg4A9/M6vfjl/oRVDc+XXSclNfaJuZiVvI67neCiP1PH8" +
+            "n03UuMoYdeyjG9Rwa7jPrX74Nn/dmFgit8A/RmWOKrGcPwAAAAASUVORK5CYII=")]
         public string RobotImage {
             get {
                 return ((string)(this["RobotImage"]));
